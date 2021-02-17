17be43e4eafe05c57db60872a7481e899c2dc915
edenpaulocruz <edenpaulo@gmail.com>
2021-02-17 01:54:25 -0300

    Admin registers a promotion

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -6,4 +6,16 @@ class PromotionsController < ApplicationController
   def show
     @promotion = Promotion.find(params[:id])
   end
+
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = Promotion.new(params.require(:promotion).permit(
+      :name, :description, :code, :discount_rate, :coupon_quantity,
+      :expiration_date))
+    @promotion.save
+    redirect_to @promotion
+  end
 end
