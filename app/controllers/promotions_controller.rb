3ee1e8e9ac18f6444dd1edc621bdb792d534bac1
edenpaulocruz <edenpaulo@gmail.com>
2021-02-18 00:33:07 -0300

    Add validation and i18n

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -4,7 +4,8 @@ class PromotionsController < ApplicationController
   end
 
   def show
-    @promotion = Promotion.find(params[:id])
+    id = params[:id]
+    @promotion = Promotion.find(id)
   end
 
   def new
@@ -12,10 +13,19 @@ class PromotionsController < ApplicationController
   end
 
   def create
-    @promotion = Promotion.new(params.require(:promotion).permit(
-      :name, :description, :code, :discount_rate, :coupon_quantity,
-      :expiration_date))
-    @promotion.save
-    redirect_to @promotion
+    @promotion = Promotion.new(promotion_params)
+    if @promotion.save
+      redirect_to @promotion
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def promotion_params
+    params.require(:promotion).permit(:name, :description, :code,
+                                      :discount_rate, :coupon_quantity,
+                                      :expiration_date)
   end
 end
