3dbf157bdb111839d02521d01763ddb88af5165a
edenpaulocruz <edenpaulo@gmail.com>
2021-02-27 18:59:47 -0300

    Burn a coupon of system

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,5 @@
 class Coupon < ApplicationRecord
   belongs_to :promotion
 
-  enum status: { active: 0, inactivate: 5 }
+  enum status: { active: 0, inactive: 5 }
 end
--- spec/features/admin_inactivates_a_coupon_spec.rb
+++ spec/features/admin_inactivates_a_coupon_spec.rb
@@ -13,6 +13,31 @@ feature 'Admin inactivates a coupon' do
     click_on promotion.name
     click_on 'Inativar'
 
+    coupon.reload
     expect(page).to have_content('NATAL10-0001 (Inativo)')
+    expect(coupon).to be_inactive #inactive?
+  end
+
+  scenario 'does not view button' do
+    promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 1,
+                      expiration_date: '22/12/2033')
+    inactive_coupon = Coupon.create!(code: 'NATAL10-0001', promotion: promotion, status: :inactive)
+    active_coupon = Coupon.create!(code: 'NATAL10-0002', promotion: promotion)
+
+    visit root_path
+    click_on 'Promoções'
+    click_on promotion.name
+
+    expect(page).to have_content('NATAL10-0001 (Inativo)')
+    expect(page).to have_content('NATAL10-0002 (Ativo)')
+    
+    within("div#coupon-#{active_coupon.id}") do
+      expect(page).to have_link 'Inativar'
+    end
+
+    within("div#coupon-#{inactive_coupon.id}") do
+      expect(page).not_to have_link 'Inativar'
+    end
   end
 end
